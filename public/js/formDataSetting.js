e4a9272511c501291f4414ecb8fc0361510b6ca3
RYANCOAL9999 <lmf242003@gmail.com>
2019-04-21 22:45:39 +0800

    form update

--- public/js/formDataSetting.js
+++ public/js/formDataSetting.js
@@ -228,7 +228,7 @@ var keepData =
     '4' :
     {
         property : '物業',
-        date : '視察日期',
+        date : '視察日期(如買方同意不視察物業，請指明)',
         relation : '代理關係',
         right : '買方放棄收取物業資料表格(包括賣方的陳述)的權利',
         user : '買方所須支付的佣金的數額或收費率',
@@ -248,7 +248,7 @@ var keepData =
     '6':
     {
         property : '物業',
-        date : '視察日期',
+        date : '視察日期(如租客同意不視察物業，請指明)',
         relation : '代理關係',
         right : '租客放棄收取出租物業資料表格的權利',
         user : '租客所須支付的佣金的數額或收費率',
@@ -291,7 +291,7 @@ var keepData =
     '6':
     {
         property : 'Properties',
-        date : 'Date of inspection (please specify if Purchaser agrees not to inspect)',
+        date : 'Date of inspection (please specify if Tenant agrees not to inspect)',
         relation : 'Agency Relationship',
         right : 'Tenant waives his right to receive Leasing Information Forms',
         user : 'Amount or rate of commission to be paid by Tenant',
